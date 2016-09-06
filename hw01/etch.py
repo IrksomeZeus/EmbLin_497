a009f94668e431ccfbc07a904f1e2027f4cd8714
Austin Yates <Yatesal@Rose-Hulman.edu>
2016-09-05 20:26:16 -0400

    Basic grid is displayed a long with instructions

--- hw01/etch.py
+++ hw01/etch.py
@@ -7,8 +7,16 @@ import curses
 
 myscreen = curses.initscr()
 
+x_grid = 8
+y_grid = 8
+
 myscreen.border(0)
-myscreen.addstr(12, 25, "Test String")
+myscreen.addstr(0, 0, "Etch-a-Sketch v1.0")
+myscreen.addstr(1, 0, "Use W, A, S, and D to move the cursor. The Spacebar clears the board.")
+for i in range(0, x_grid):
+    myscreen.addstr(3, (i*2) + 2, str(i))
+for j in range(0, y_grid):
+    myscreen.addstr(j + 4, 1, str(j))
 myscreen.refresh()
 myscreen.getch()
 
