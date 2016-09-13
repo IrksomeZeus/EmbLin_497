a8049c31d09e7b0c93123e1db4cb9bac2bda1829
Austin Yates <Yatesal@Rose-Hulman.edu>
2016-09-13 14:53:06 -0400

    Program can now be controlled using the buttons attached to the gpio

--- hw02/etch.py
+++ hw02/etch.py
@@ -4,18 +4,7 @@
 # Austin Yates  Sept. 5, 2016
 
 import curses
-
-# initializes the window
-myscreen = curses.initscr()
-curses.noecho()
-curses.cbreak()
-curses.curs_set(0)
-
-x_grid = 8
-y_grid = 8
-
-cur_x = 0
-cur_y = 0
+import Adafruit_BBIO.GPIO as GPIO
 
 def cursor_home():
     # Moves the cursor to the home position in the game.
@@ -30,6 +19,10 @@ def move_cursor(dy, dx):
     # Moves the cursor as directed and writes an X at the new location
     global cur_x
     global cur_y
+    global cleared
+    global reset
+    cleared = 0
+    reset = 0
     dx = 2 * dx
     if (cur_y + dy >= y_grid) or (cur_y + dy < 0):
         return
@@ -47,6 +40,8 @@ def size_prompt():
     global x_grid
 
     curses.echo()
+    curses.nocbreak()
+
     myscreen.clear()
     myscreen.border(0)
     myscreen.addstr(0, 0, "Etch-a-Sketch v0.2")
@@ -56,10 +51,14 @@ def size_prompt():
     myscreen.addstr(5, 1, "Please enter the X grid size: ")
     myscreen.refresh()
     x_grid = int(myscreen.getstr())
+
+    curses.cbreak()
     curses.noecho()
 
 def reset_board():
     # Resets board to the initial start conditions.
+    global reset
+    reset = 1
     size_prompt()
 
     myscreen.clear()
@@ -76,6 +75,8 @@ def reset_board():
 
 def clear_board():
     # clears the board while keeping the cursor in the same place
+    global cleared
+    cleared = 1
     for k in range(0, y_grid):
         myscreen.move(k+4, 3)
         myscreen.clrtoeol()
@@ -83,10 +84,62 @@ def clear_board():
     myscreen.addstr('X')
     myscreen.refresh()
 
+def clearHandler(channel):
+    clear_board()
+
+def moveLeft(channel):
+    move_cursor(0, -1)
+
+def moveRight(channel):
+    move_cursor(0, 1)
+
+def moveUp(channel):
+    move_cursor(-1, 0)
+
+def moveDown(channel):
+    move_cursor(1, 0)
+
+
+myscreen = curses.initscr()
+curses.noecho()
+curses.cbreak()
+curses.curs_set(0)
+
+x_grid = 8
+y_grid = 8
+
+cur_x = 0
+cur_y = 0
+
+cleared = 0
+
+#setting descriptive names to pins being used
+LEFT_BTN = "P9_11"
+UP_BTN = "P9_13"
+RIGHT_BTN = "P9_15"
+DOWN_BTN = "P9_17"
+CLEAR_BTN = "P9_18"
+
+#setting up inputs
+GPIO.setup(LEFT_BTN, GPIO.IN)
+GPIO.setup(RIGHT_BTN, GPIO.IN)
+GPIO.setup(UP_BTN, GPIO.IN)
+GPIO.setup(DOWN_BTN, GPIO.IN)
+GPIO.setup(CLEAR_BTN, GPIO.IN)
+
+#setting up event detection
+GPIO.add_event_detect(LEFT_BTN, GPIO.FALLING, callback=moveLeft, bouncetime=200)
+GPIO.add_event_detect(RIGHT_BTN, GPIO.FALLING, callback=moveRight, bouncetime=200)
+GPIO.add_event_detect(UP_BTN, GPIO.FALLING, callback=moveUp, bouncetime=200)
+GPIO.add_event_detect(DOWN_BTN, GPIO.FALLING, callback=moveDown, bouncetime=200)
+GPIO.add_event_detect(CLEAR_BTN, GPIO.FALLING, callback=clearHandler, bouncetime=200)
+
 reset_board()
 
 x = 0
-while x != ord('q'):
+keep_going = 1
+
+while keep_going:
     x = myscreen.getch()
 
     if x == ord('w'):
@@ -98,11 +151,16 @@ while x != ord('q'):
     if x == ord('d'):
         move_cursor(0, 1)
     if x == ord('c'):
-        clear_board()
+        clearHandler(None)
     if x == ord('r'):
         reset_board()
+    if x == ord('q'):
+        keep_going = 0
 
-# restore terminal settings and exit
-curses.nocbreak()
+# Normal Exit
+
 curses.echo()
+curses.nocbreak()
 curses.endwin()
+# cleanup GPIO
+GPIO.cleanup()
