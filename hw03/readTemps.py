c6790457a7083f2015d6571874c369bdf14bc599
Austin Yates <Yatesal@Rose-Hulman.edu>
2016-10-03 23:24:24 -0400

    Now reads and displays the value from both Temp101 sensors.

--- hw03/readTemps.py
+++ hw03/readTemps.py
@@ -8,7 +8,7 @@ from Adafruit_I2C import Adafruit_I2C
 TEMP_REG = 0
 
 temp101_0 = Adafruit_I2C(0x48, 2)
-temp101_1 = Adafruit_I2C(0x49)
+temp101_1 = Adafruit_I2C(0x49, 2)
 temp006_0 = Adafruit_I2C(0x40)
 
 def toFahrenheit(celsius):
@@ -16,7 +16,12 @@ def toFahrenheit(celsius):
     return f_temp
 
 temp_0 = temp101_0.readS16(TEMP_REG)
-print 'Raw value read: {}'.format(temp_0)
-
+temp_0 = temp_0 & 0x01ff
 temp_0 = toFahrenheit(temp_0)
-print 'Temp in F: {}'.format(temp_0)
+
+temp_1 = temp101_1.readS16(TEMP_REG)
+temp_1 = temp_1 & 0x01ff
+temp_1 = toFahrenheit(temp_1)
+
+print 'Sensor 0: {}F'.format(temp_0)
+print 'Sensor 1: {}F'.format(temp_1)
